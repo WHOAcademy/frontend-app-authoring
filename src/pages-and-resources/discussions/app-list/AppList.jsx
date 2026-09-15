c846b4f130808bac593bdbe08b4c0d2e6eee7e71
rp-who <raj.pipaliya@who.int>
2026-09-15 14:55:26 +0530

    fix: removed extra discussion tools

--- src/pages-and-resources/discussions/app-list/AppList.jsx
+++ src/pages-and-resources/discussions/app-list/AppList.jsx
@@ -30,7 +30,7 @@ const AppList = () => {
     appIds, featureIds, status, saveStatus, activeAppId, selectedAppId, enabled, postingRestrictions,
   } = useSelector(state => state.discussions);
   const [discussionTabToggle, setDiscussionTabToggle] = useState(enabled);
-  const apps = useModels('apps', appIds);
+  const apps = useModels('apps', appIds).filter(app => app.id === 'legacy');
   const features = useModels('features', featureIds);
   const isGlobalStaff = getAuthenticatedUser().administrator;
   const ltiProvider = !['openedx', 'legacy'].includes(activeAppId);
